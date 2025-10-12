3a2aa5063bc441a6e4dda71460096b023915b414
hoboJF <jadenfiero@gmail.com>
2025-10-11 23:14:39 -0400

    added simple code to read input from a file into a string, also added one test case to make sure functionality is as intended

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2{
+        eprintln!("usage: {} <input_file>", args[0]);
+        std::process::exit(1);
+    }
+    let filename = &args[1];
+    let lolspeak_string = fs::read_to_string(filename).unwrap_or_else(|err| {
+        eprintln!("error reading file '{}': {}", filename, err);
+        std::process::exit(1);
+    });
+    println!("{}", lolspeak_string);
 }
